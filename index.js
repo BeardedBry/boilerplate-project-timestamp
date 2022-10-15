bc2ac6db003f8e24cf9eb6fdb81438b74c50db7f
Brian Burkard <brianburkard@gmail.com>
2022-10-15 17:08:18 -0400

    :feat: rename 'ip' key to 'ipaddress'

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/api/whoami", function (req, res) {
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
   // console.log('req', req);
-  res.json({'ip': ip, 'language': language, 'software': software})
+  res.json({'ipaddress': ip, 'language': language, 'software': software})
 });
 
 app.get("/api/:date([0-9]{4}[\/\-][0-9]{2}[\/\-][0-9]{2})", function (req, res) {
